5c023c172aba5f1da3b0f85981587aa61f8c972c
Sachin-kalaiselvan <161479509+Sachin-kalaiselvan@users.noreply.github.com>
2026-05-28 00:58:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackStartVite } from "@tanstack/react-start/vite";
+import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    TanStackStartVite({
-      server: {
-        entry: "src/server.ts",
-      },
-    }),
+    TanStackStartVite(),
     react(),
     tailwindcss(),
     tsConfigPaths(),
