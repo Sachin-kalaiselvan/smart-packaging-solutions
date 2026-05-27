bdb87d8cbd9d11ef7ef5abab49184ddd2462b7e8
Sachin-kalaiselvan <161479509+Sachin-kalaiselvan@users.noreply.github.com>
2026-05-28 02:00:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   root: "src",
   plugins: [
+    tanstackStart(),
     react(),
     tailwindcss(),
     tsConfigPaths(),
@@ -14,7 +16,7 @@ export default defineConfig({
     noExternal: ["leaflet"],
   },
   build: {
-    outDir: "../dist",
+    outDir: "../dist/client",
     emptyOutDir: true,
     chunkSizeWarningLimit: 1500,
   },
